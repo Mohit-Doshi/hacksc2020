5847cac0b183e7cb926daf7b14d32346242c68bc
Ramkumar <ramkumar.kr94@gmail.com>
2020-02-01 23:28:47 -0800

    Add points

--- frontend/import-svrm/src/pages/points.js
+++ frontend/import-svrm/src/pages/points.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Menu from 'components/Menu';
+import firebaseWrapper from '../firebaseWrapper';
 
 ReactDOM.render(<Menu />, document.getElementById('menu'));
+firebaseWrapper.getUser(localStorage.getItem("userid")).then((userid) => {
+    document.getElementById("pointNum").innerText = userid.points
+})
